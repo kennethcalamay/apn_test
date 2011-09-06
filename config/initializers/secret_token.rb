11642dde4c33b57815562ea18ee1c562bb101a10
Kenneth Calamay <github@k.calamay.me>
2011-09-06 11:46:43 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ApnTest::Application.config.secret_token = 'b22fffde4f2ba0c661f5630b6c7d1773b2b04615f5aaeb2d52805440f9180775399efb3ae833911de1acbfe1925d2c25a1ec6b532554e28d451386da6678a591'
